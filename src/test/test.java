5630195703e3743e42bf3c1cb20a36934b7b7eed
fire-hot <yn__818@163.com>
2017-06-09 21:13:08 +0800

    你好

--- src/test/test.java
+++ src/test/test.java
@@ -3,8 +3,11 @@ package test;
 public class test {
     public static void main(String[] args){
         System.out.println("aaa");
+        show();
     }
-    public void show(){
+    public static void show(){
         System.out.println("8888888888888");
+        System.out.println("你好");
     }
+
 }
