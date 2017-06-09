92a6d8aaa88b540931b1274fdd586125d4a89807
f1 <164353027@qq.com>
2017-06-09 20:56:34 +0800

    show()

--- src/test/test.java
+++ src/test/test.java
@@ -4,4 +4,7 @@ public class test {
     public static void main(String[] args){
         System.out.println("aaa");
     }
+    public void show(){
+        System.out.println("8888888888888");
+    }
 }
